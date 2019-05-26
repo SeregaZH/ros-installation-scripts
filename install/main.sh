ca3e81fe315c94efeb0a2915c76f65fdacfe4899
SeregaZH <zhelezko1990@gmail.com>
2019-05-26 22:40:03 +0300

    make script path relative to run directory

--- install/main.sh
+++ install/main.sh
@@ -3,7 +3,8 @@
 BASE_DIR='/home/main'
 TRTL_PATH='/catkin_ws'
 
-sudo bash ./ros-install.sh
+DIR="$(dirname "$0")"
+sudo bash ${DIR}/ros-install.sh
 
 REBUILD_PX4=${1:-keep}
 if [[ ${REBUILD_PX4} = rebuild ]]
@@ -19,9 +20,9 @@ fi
 if [[ ${REBUILD_PX4} = rebuild ]]
 then
    TRTL_FULL_PATH="$BASE_DIR$TRTL_PATH"
-   sudo bash ./px4-sitl-install.sh ${BASE_DIR}
-   sudo bash ./catkin-install.sh empty ${TRTL_FULL_PATH}
-   sudo bash ./turtlebot-install.sh ${TRTL_FULL_PATH}
+   sudo bash ${DIR}/px4-sitl-install.sh ${BASE_DIR}
+   sudo bash ${DIR}/catkin-install.sh empty ${TRTL_FULL_PATH}
+   sudo bash ${DIR}/turtlebot-install.sh ${TRTL_FULL_PATH}
 fi
 
-sudo bash ./ros-bridge-install.sh
+sudo bash ${DIR}/ros-bridge-install.sh
