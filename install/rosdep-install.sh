3e00516df2f8cbf73d3aa9f9f6df3fc74ac9259b
SeregaZH <zhelezko1990@gmail.com>
2019-07-31 21:00:37 +0300

    install ros web video server

--- install/rosdep-install.sh
+++ install/rosdep-install.sh
@@ -4,7 +4,8 @@ ROS_DISRTO=${1:-'melodic'}
 
 sudo apt-get install -y ros-${ROS_DISRTO}-nodelet \
                         ros-${ROS_DISRTO}-image-transport \
-                        ros-${ROS_DISRTO}-dynamic-reconfigure
+                        ros-${ROS_DISRTO}-dynamic-reconfigure \
+                        ros-${ROS_DISRTO}-web-video-server
 
 sudo apt-key adv --keyserver keys.gnupg.net --recv-key C8B3A55A6F3EFCDE \
      || sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-key C8B3A55A6F3EFCDE
